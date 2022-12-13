d924a40da801acf5ea914a09b0f82f2844fa145a
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-12-13 20:12:42 +0600

    delete operation done form server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 // mongoCrud
@@ -19,11 +19,30 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const userCollection = client.db('nodeMongoCrud').collection('users');
+
+        // R - Read of CRUD operations
+        app.get('/users', async (req, res) =>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        // C- Create of CRUD operations
         app.post('/users', async (req, res)=>{
             const user = req.body;
             console.log(user);
             const result = await userCollection.insertOne(user)
             res.send(result);
+        })
+
+        // D - Delete of CRUD operations
+        app.delete('/users/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            // console.log('Trying to delete', id)
+            res.send(result)
         })
     }
     finally{
